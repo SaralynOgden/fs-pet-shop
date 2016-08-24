846d7b7f02e01fb1f4e26bd4d544387f12cb2bbf
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-23 20:44:25 -0600

    fix conditionals to exclude indeces that are not numbers

--- pets.js
+++ pets.js
@@ -1,44 +1,28 @@
 'use strict';
 
-//import required libraries
 var fs = require('fs');
 var path = require('path');
-
-//pets path is the current directory and the
 var petsPath = path.join(__dirname, 'pets.json');
-
-//variable to store the last portion of the executable path
 var node = path.basename(process.argv[0]);
-
-//variable to store the last portion of the javascript file being executed
 var file = path.basename(process.argv[1]);
-
-//variable to store the command sent through terminal
 var cmd = process.argv[2];
-
-//variable to store the index sent through terminal
 var index = process.argv[3];
 var age = index
 var kind = process.argv[4]
 var name = process.argv[5]
 
-//if the command sent through the terminal is 'read', read the file and log it to the terminal
 fs.readFile(petsPath, 'utf8', function(err, data) {
   var pets = JSON.parse(data);
   if (err) {
     throw err;
   }
-
   if(cmd === 'create' && (age === undefined || kind === undefined || name === undefined)){
     console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`)
     process.exit(1)
-  } else {
+  } else if (cmd === 'create' && age !== undefined && kind !== undefined && name !== undefined){
     console.log({age: age, kind: kind, name: name})
     process.exit(0)
-  }
-
-
-  if (cmd === 'read' && index > pets.length - 1 || index < 0){
+  } else if (cmd === 'read' && (index > pets.length - 1 || index < 0) || isNaN(index)){
     console.error(`Usage: ${node} ${file} ${cmd} INDEX`)
     process.exit(9)
   } else if (cmd === 'read' && index < pets.length){
