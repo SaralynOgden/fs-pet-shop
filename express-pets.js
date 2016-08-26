10a76dff35d57942c49ec8ab9d10f6982ef3475c
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-25 22:17:04 -0600

    add middlware and make server restful?

--- express-pets.js
+++ express-pets.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 var pets = require('./pets.json')
+app.use(require('morgan')('dev'));
+app.use(require('body-parser').json());
 
 app.get('/pets', (req, res, next) => {
   res.status(200).send(pets)
@@ -18,17 +20,39 @@ app.get('/*', (req, res, next) => {
 })
 
 app.post('/pets', function (req, res, next) {
-  let body = [];
-  req.on('data', function (chunk) {
-    body.push(chunk.toString());
-  }).on('end', function () {
-    let data = JSON.parse(body.join(''));
-    !data.age || !data.kind || !data.name ? (
-      res.contentType('text/plain'),
-      res.status(400).send('Bad Request')) : (
-      pets.push(data),
-      res.status(201).send(pets))
-  });
+  let body = req.body
+  !body.age || !body.kind || !body.name ? (
+    res.contentType('text/plain'),
+    res.status(400).send('Bad Request')) : (
+    pets.push(body),
+    res.status(201).send(pets))
+});
+
+app.put('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+  if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    return res.status(404).send({
+      message: 'Index of pets not found',
+      data: index
+    });
+  }
+  var pet = req.body;
+  console.log(pet);
+  if (!pet) {
+    return res.sendStatus(400);
+  }
+  pets[index] = pet;
+
+  res.send(pet);
+});
+
+app.delete('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+  if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    return res.sendStatus(404);
+  }
+  var pet = pets.splice(index, 1)[0];
+  res.send(pet);
 });
 
 app.listen(8000, () => {
