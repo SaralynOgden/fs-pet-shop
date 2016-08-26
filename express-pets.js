ab32f7db8e545599451682715732049b7c783b00
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-25 22:32:33 -0600

    tweak some code

--- express-pets.js
+++ express-pets.js
@@ -1,34 +1,44 @@
 var express = require('express')
 var app = express()
 var pets = require('./pets.json')
+
 app.use(require('morgan')('dev'));
 app.use(require('body-parser').json());
 
-app.get('/pets', (req, res, next) => {
-  res.status(200).send(pets)
-})
+app.get('/pets', getPets)
+app.get('/pets/:index', getSinglePet)
+app.get('/*', handleInvalidInput)
+app.post('/pets', postPet)
+app.put('/pets/:index', updatePet)
+app.delete('/pets/:index', removePet)
 
-app.get('/pets/:index', (req, res, next) => {
+function getPets (req, res, next) {
+  res.status(200).send(pets)
+}
+
+function getSinglePet (req, res, next) {
   pets[req.params.index] ? (
   res.status(200).send(pets[req.params.index])) : (
   res.contentType('text/plain'),
   res.status(404).send('Not Found'))
-})
+}
 
-app.get('/*', (req, res, next) => {
+function handleInvalidInput (req, res, next) {
   (res.status(404).send('Not Found'))
-})
+}
 
-app.post('/pets', function (req, res, next) {
+function postPet (req, res, next) {
   let body = req.body
-  !body.age || !body.kind || !body.name ? (
-    res.contentType('text/plain'),
-    res.status(400).send('Bad Request')) : (
-    pets.push(body),
-    res.status(201).send(pets))
-});
+  if(!body.age || !body.kind || !body.name) {
+    res.contentType('text/plain')
+    res.status(400).send('Bad Request')
+  } else {
+    pets.push(body)
+    res.status(201).send(pets)
+  }
+}
 
-app.put('/pets/:index', function(req, res) {
+function updatePet (req, res) {
   var index = Number.parseInt(req.params.index);
   if (Number.isNaN(index) || index < 0 || index >= pets.length) {
     return res.status(404).send({
@@ -42,18 +52,17 @@ app.put('/pets/:index', function(req, res) {
     return res.sendStatus(400);
   }
   pets[index] = pet;
-
   res.send(pet);
-});
+}
 
-app.delete('/pets/:index', function(req, res) {
+function removePet (req, res) {
   var index = Number.parseInt(req.params.index);
   if (Number.isNaN(index) || index < 0 || index >= pets.length) {
     return res.sendStatus(404);
   }
   var pet = pets.splice(index, 1)[0];
   res.send(pet);
-});
+}
 
 app.listen(8000, () => {
   console.log('listening on 8000');
